0a250e024942bdcece09b0716ebbbafaf7c85f27
Greg D. Hill <greghill@cs.stanford.edu>
2016-04-01 23:38:08 +0000

    add cloud connect script

--- cloud-connect.sh
+++ cloud-connect.sh
@@ -1,6 +1,6 @@
-if [ "$#" -ne 1 ]; then
-echo "Usage: $0 + [hostname]"
+if [ "$#" -ne 2 ]; then
+echo "Usage: $0 [username] [hostname]"
 exit 1
 fi
-FOREIGN_HOSTNAME=$1
-RANDOM=$FOREIGN_HOSTNAME; PORT=$((RANDOM + 2000)); echo "Trying to connect to hostname $FOREIGN_HOSTNAME which we expect to be accessable on local port $PORT"; ssh -p $PORT localhost
+FOREIGN_HOSTNAME=$2
+RANDOM=$FOREIGN_HOSTNAME; PORT=$((RANDOM + 2000)); echo "Trying to connect to hostname $FOREIGN_HOSTNAME which we expect to be accessable on local port $PORT"; ssh -p $PORT $1@localhost
