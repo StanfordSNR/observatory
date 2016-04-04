394ef440ab4ff3c998dbf8c017e61e0f288e18f3
Greg D. Hill <greghill@cs.stanford.edu>
2016-04-04 16:03:36 -0700

    Screen called inside autossh-screipt file. Added second screen to connect to pion.

--- autossh-script.sh
+++ autossh-script.sh
@@ -3,4 +3,5 @@
 export AUTOSSH_GATETIME=0
 PORT=$(( (0x`tr "[:upper:]" "[:lower:]" <<< $HOSTNAME | md5sum | cut -c -8` % 61991) + 2000 )) # 2000 min port, 61991 random prime
 echo "Forwarding on server port $PORT"
-autossh greg@104.154.52.255 -R $PORT:localhost:22
+screen -dmS autossh-glcoud autossh greg@104.154.52.255 -R $PORT:localhost:22
+screen -dmS autossh-stanford autossh greg@pion.stanford.edu -R $PORT:localhost:22
