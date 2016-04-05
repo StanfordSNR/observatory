2babd9270770f94841ae7fc670c042d82f11afae
Greg D. Hill <greghill@cs.stanford.edu>
2016-04-05 21:46:22 +0000

    Ports are variables

--- autossh-script.sh
+++ autossh-script.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+GCLOUD_PORT=58100
+PION_PORT=58104
 export AUTOSSH_GATETIME=0
-screen -dmS autossh-glcoud autossh lpng@104.196.19.245 -R 58100:localhost:22
-screen -dmS autossh-stanford autossh lpng@pion.stanford.edu -R 58104:localhost:22
+screen -dmS autossh-glcoud autossh lpng@104.196.19.245 -R $GCLOUD_PORT:localhost:22
+screen -dmS autossh-stanford autossh lpng@pion.stanford.edu -R $PION_PORT:localhost:22
