6d65a21a23b28754963acacba6f04d77b29be4c9
Greg D. Hill <greghill@cs.stanford.edu>
2016-04-06 16:35:17 -0700

    Back to hash based port selection for now, with better comment.

--- autossh-script.sh
+++ autossh-script.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-GCLOUD_PORT=58100
-PION_PORT=58104
-AWS_PORT=58108
 export AUTOSSH_GATETIME=0
-screen -dmS autossh-glcoud autossh lpng@104.196.19.245 -R $GCLOUD_PORT:localhost:22
-screen -dmS autossh-stanford autossh lpng@pion.stanford.edu -R $PION_PORT:localhost:22
-screen -dmS autossh-aws autossh lpng@52.9.177.212 -R $AWS_PORT:localhost:22
+
+# choose port between 2000, 65499 based on the first 64 bits of the md5sum of the case-insensitive hostname (63499 is a prime number)
+PORT=$(( (0x`tr "[:upper:]" "[:lower:]" <<< $HOSTNAME | md5sum | cut -c -16` % 63499) + 2000 ))
+
+screen -dmS autossh-glcoud autossh lpng@104.196.19.245 -R $PORT:localhost:22
+screen -dmS autossh-stanford autossh lpng@pion.stanford.edu -R $PORT:localhost:22
+screen -dmS autossh-aws autossh lpng@52.9.177.212 -R $PORT:localhost:22
--- cloud-connect.sh
+++ cloud-connect.sh
@@ -1,6 +1,10 @@
 if [ "$#" -ne 2 ]; then
-echo "Usage: $0 [username] [port]"
+echo "Usage: $0 [username] [hostname]"
 exit 1
 fi
-echo "Trying to connect to hostname $1 which we expect to be accessable on local port $2"
-ssh -p $2 $1@localhost
+
+# choose port between 2000, 65499 based on the first 64 bits of the md5sum of the case-insensitive hostname (63499 is a prime number)
+PORT=$(( (0x`tr "[:upper:]" "[:lower:]" <<< $HOSTNAME | md5sum | cut -c -16` % 63499) + 2000 ))
+
+echo "Trying to connect to hostname $2 which we expect to be accessable on local port $port"
+ssh $1@localhost:$PORT
