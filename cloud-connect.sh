9631629d9fde3f53413dc557b481506abcc1d5d8
Greg D. Hill <greghill@cs.stanford.edu>
2016-04-07 17:46:03 +0000

    typo

--- cloud-connect.sh
+++ cloud-connect.sh
@@ -6,5 +6,5 @@ fi
 # choose port between 2000, 65499 based on the first 64 bits of the md5sum of the case-insensitive hostname (63499 is a prime number)
 PORT=$(( (0x`tr "[:upper:]" "[:lower:]" <<< $HOSTNAME | md5sum | cut -c -16` % 63499) + 2000 ))
 
-echo "Trying to connect to hostname $2 which we expect to be accessable on local port $port"
+echo "Trying to connect to hostname $2 which we expect to be accessable on local port $PORT"
 ssh $1@localhost:$PORT
