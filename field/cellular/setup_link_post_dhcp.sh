939173f00a959cc06727133aead8ccff0253b94f
Greg D. Hill <greghill@cs.stanford.edu>
2016-04-25 21:11:37 -0700

    rename link setup script, only take head from DHCP req

--- field/cellular/setup_link_post_dhcp.sh
+++ field/cellular/setup_link_post_dhcp.sh
@@ -1,4 +1,4 @@
-RESPONSE=`cat temp_at_dhcp | grep DHCP: | cut -d: -f 2`
+RESPONSE=`cat temp_at_dhcp | grep DHCP: | head | cut -d: -f 2`
 UNPACKED_RESPONSE=`perl -e 'print join(",",map { join(".", unpack("C4", pack("L", hex))) } split /,/, shift),"\n"' $RESPONSE`
 IP=`echo $UNPACKED_RESPONSE | cut -d, -f 1`
 MASK=`echo $UNPACKED_RESPONSE | cut -d, -f 2`
