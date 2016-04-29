95315e61210c07abeaa6f1082a52d529f261c252
Greg D. Hill <greghill@cs.stanford.edu>
2016-04-29 14:45:17 -0700

    improve scripts

--- field/cellular/setup_link_post_dhcp.sh
+++ field/cellular/setup_link_post_dhcp.sh
@@ -1,9 +1,21 @@
 RESPONSE=`cat temp_at_dhcp | grep DHCP: | head | cut -d: -f 2`
-UNPACKED_RESPONSE=`perl -e 'print join(",",map { join(".", unpack("C4", pack("L", hex))) } split /,/, shift),"\n"' $RESPONSE`
+UNPACKED_RESPONSE=`perl -e 'print join(", ",map { join(".", unpack("C4", pack("L", hex))) } split /,/, shift),"\n"' $RESPONSE`
 IP=`echo $UNPACKED_RESPONSE | cut -d, -f 1`
 MASK=`echo $UNPACKED_RESPONSE | cut -d, -f 2`
 VIA=`echo $UNPACKED_RESPONSE | cut -d, -f 3`
+DNS1=`echo $UNPACKED_RESPONSE | cut -d, -f 5`
+DNS2=`echo $UNPACKED_RESPONSE | cut -d, -f 6`
+
 echo "got $UNPACKED_RESPONSE"
-sudo ifconfig wwan0 $IP netmask $MASK
-sudo ip route add default via $VIA
-echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" | sudo resolvconf -a wwan0
+
+IFACE_CMD="sudo ifconfig wwan0 $IP netmask $MASK"
+echo $IFACE_CMD
+$IFACE_CMD
+
+ROUTE_CMD="sudo ip route add default via $VIA"
+echo $ROUTE_CMD
+$ROUTE_CMD
+
+DNS_ENTRIES="nameserver $DNS1\nnameserver $DNS2\n"
+echo -e "adding \n$DNS_ENTRIES to resolvconf"
+echo -e $DNS_ENTRIES | sudo resolvconf -a wwan0
