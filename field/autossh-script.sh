6fb6b1bc84779b5eecac516e50eb2f77c9eb5326
Greg D. Hill <greghill@cs.stanford.edu>
2016-04-29 18:31:44 -0700

    fixing script bugs

--- field/autossh-script.sh
+++ field/autossh-script.sh
@@ -2,6 +2,6 @@
 
 export AUTOSSH_GATETIME=0
 
-PORT=$(/home/lpng/diagnostic_box_scripts/field/machine_to_port.py $HOSTNAME)
+PORT=$(/home/pi/diagnostic_box_scripts/field/machine_to_port.py $HOSTNAME)
 screen -dmS autossh-gcloud autossh lpng@104.196.19.245 -R $PORT:localhost:22
 screen -dmS autossh-aws autossh lpng@52.9.177.212 -R $PORT:localhost:22
--- field/cellular/automated_connection.sh
+++ field/cellular/automated_connection.sh
@@ -1,8 +1,13 @@
 while : ; do
+    while ! lsusb | grep -i huawei;
+    do
+        echo "no huawei devices found"
+        sleep 1s
+    done
     echo sleep 5s
     sleep 5s
-    echo ./generic_huawei_modeswitch
-    ./generic_huawei_modeswitch
+    echo generic_huawei_modeswitch
+    /home/pi/diagnostic_box_scripts/field/cellular/generic_huawei_modeswitch
     echo sleep 5s
     sleep 5s
     echo sudo wvdial E397
