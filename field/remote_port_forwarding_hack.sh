2f766152b7658acefcd03bc54779accf102b537d
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-04 04:26:14 -0700

    fixed remote port forwarding hack

--- field/remote_port_forwarding_hack.sh
+++ field/remote_port_forwarding_hack.sh
@@ -1,4 +1,4 @@
 echo -n "got "
-cat ~/screenlog.* | grep "Allocated port" | tail -n 1 | cut -d ' '  -f 3 | cat
-cat ~/screenlog.* | grep "Allocated port" | tail -n 1 | cut -d ' '  -f 3 | ssh lpng@52.9.177.212 'cat > diagnostic_box_scripts/cloud/port'
-cat ~/screenlog.* | grep "Allocated port" | tail -n 1 | cut -d ' '  -f 3 | ssh lpng@104.196.19.245 'cat > diagnostic_box_scripts/cloud/port'
+cat ~/screenlog.* | grep -o "Allocated port [0-9]*" | tail -n 1 | cut -d ' '  -f 3 | cat
+cat ~/screenlog.* | grep -o "Allocated port [0-9]*" | tail -n 1 | cut -d ' '  -f 3 | ssh lpng@52.9.177.212 'cat > diagnostic_box_scripts/cloud/port'
+cat ~/screenlog.* | grep -o "Allocated port [0-9]*" | tail -n 1 | cut -d ' '  -f 3 | ssh lpng@104.196.19.245 'cat > diagnostic_box_scripts/cloud/port'
