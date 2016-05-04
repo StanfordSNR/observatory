fc46bc02146d88b7f3e61c17aeff97ac7baffdf1
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-03 21:45:03 -0700

    fixed cloud connec script. Add git pull script

--- cloud/connect-from-cloud.sh
+++ cloud/connect-from-cloud.sh
@@ -5,4 +5,4 @@ fi
 
 PORT=$(/home/lpng/diagnostic_box_scripts/field/machine_to_port.py $1)
 echo "Trying to connect to hostname $1 which we expect to be accessable on local port $PORT"
-ssh -p $PORT lpng@localhost
+ssh -p $PORT pi@localhost
---
+++ home/git_pull_on_gcloud_and_aws.sh
@@ -0,0 +1,3 @@
+COMMAND="cd diagnostic_box_scripts && git pull"
+ssh lpng@52.9.177.212 $COMMAND
+ssh lpng@104.196.19.245 $COMMAND
