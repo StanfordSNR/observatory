2be8f350e0a4fd77ab9732db511fdb8c9edfafc8
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-04 11:47:27 +0000

    bug

--- cloud/new_connect_from_cloud.sh
+++ cloud/new_connect_from_cloud.sh
@@ -3,6 +3,6 @@ echo "Usage: $0 [hostname]"
 exit 1
 fi
 
-PORT=$(cat /home/lpng/diagnostic_box_scripts/field/port)
+PORT=$(cat /home/lpng/diagnostic_box_scripts/cloud/port)
 echo "Trying to connect to hostname $1 which we expect to be accessable on local port $PORT"
 ssh -p $PORT pi@localhost
