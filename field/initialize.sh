74f27d8957a94b2c5a46db6fea1ac4e9fefd4b9f
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-04 01:20:49 -0700

    initialize.sh: mark as old and add simple things to do in future version

--- field/initialize.sh
+++ field/initialize.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-cd ~ && sudo apt-get install -y vim git screen autossh wicd-curses && git clone https://github.com:StanfordLPNG/diagnostic_box_scripts && sudo patch /etc/rc.local ~/diagnostic_box_scripts/field/rc.local-patch
+#this is old XXX cd ~ && sudo apt-get install -y vim git screen autossh wicd-curses && git clone https://github.com:StanfordLPNG/diagnostic_box_scripts && sudo patch /etc/rc.local ~/diagnostic_box_scripts/field/rc.local-patch
 
 #TODO:
-#make username lpng
 #raspi-config -- expand disk space
 #change password
+#ssh-known hosts and authorized_keys
