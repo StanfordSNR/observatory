cd5a6952a74ff041ceddbc58d94ce9ecdeedcd46
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-04 04:43:34 -0700

    change port forwarding hack to constant loop

--- field/remote_port_forwarding_hack.sh
+++ field/remote_port_forwarding_hack.sh
@@ -1,4 +1,12 @@
-echo -n "got "
-cat ~/screenlog.* | grep -o "Allocated port [0-9]*" | tail -n 1 | cut -d ' '  -f 3 | cat
-cat ~/screenlog.* | grep -o "Allocated port [0-9]*" | tail -n 1 | cut -d ' '  -f 3 | ssh lpng@52.9.177.212 'cat > diagnostic_box_scripts/cloud/port'
-cat ~/screenlog.* | grep -o "Allocated port [0-9]*" | tail -n 1 | cut -d ' '  -f 3 | ssh lpng@104.196.19.245 'cat > diagnostic_box_scripts/cloud/port'
+OLD_PORT=0
+while : ; do
+    PORT=$(cat ~/screenlog.* | grep -o "Allocated port [0-9]*" | tail -n 1 | cut -d ' '  -f 3 | cat)
+    echo "got $PORT"
+    if [ "$OLD_PORT" != "$PORT" ]
+    then
+        echo $PORT | ssh lpng@52.9.177.212   'cat > diagnostic_box_scripts/cloud/port'
+        echo $PORT | ssh lpng@104.196.19.245 'cat > diagnostic_box_scripts/cloud/port'
+    fi
+    OLD_PORT=$PORT
+    sleep 2
+done
