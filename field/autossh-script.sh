05652990901d83846656a4c4cc4d02797d986368
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-05 23:20:03 -0700

    Undo previous commit but also get rid of hardcoded port mapping

--- field/autossh-script.sh
+++ field/autossh-script.sh
@@ -2,6 +2,5 @@
 
 export AUTOSSH_GATETIME=0
 
-LOG_LOC_PREFIX="/home/pi/logfiles/screen/`date --iso-8601=minutes`"
-screen -dmSL autossh-gcloud "script '$LOG_LOC_PREFIX'_gcloud; autossh lpng@104.196.19.245 -R 0:localhost:22"
-screen -dmSL autossh-aws "'$LOG_LOC_PREFIX'_aws; autossh lpng@52.9.177.212 -R 0:localhost:22"
+screen -dmSL autossh-gcloud autossh lpng@104.196.19.245 -R 0:localhost:22
+screen -dmSL autossh-aws autossh lpng@52.9.177.212 -R 0:localhost:22
