4c23cc28d6edbb3545c6c99dc3bef9513e065a44
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-06 01:31:12 -0700

    multiport sending from pi

--- field/remote_port_forwarding_hack.sh
+++ field/remote_port_forwarding_hack.sh
@@ -1,12 +1,12 @@
-OLD_PORT=0
+OLD_PORTS=0
 while : ; do
-    PORT=$(cat ~/screenlog.* | grep -o "Allocated port [0-9]*" | tail -n 1 | cut -d ' '  -f 3 | cat)
-    echo "got $PORT"
-    if [ "$OLD_PORT" != "$PORT" ]
+    PORTS=$(tac ~/screenlog.* | grep -o "Allocated port [0-9]*" -m 2 | cut -d ' '  -f 3)
+    echo "got $PORTS"
+    if [ "$OLD_PORTS" != "$PORTS" ]
     then
-        echo $PORT | ssh lpng@52.9.177.212   'cat > diagnostic_box_scripts/cloud/port'
-        echo $PORT | ssh lpng@104.196.19.245 'cat > diagnostic_box_scripts/cloud/port'
+        echo $PORTS | ssh lpng@52.9.177.212   'cat > diagnostic_box_scripts/cloud/ports'
+        echo $PORTS | ssh lpng@104.196.19.245 'cat > diagnostic_box_scripts/cloud/ports'
     fi
-    OLD_PORT=$PORT
+    OLD_PORTS=$PORTS
     sleep 2
 done
