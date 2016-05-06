27464ccb90f4c4408f10b5b8c92dbad6f42bd52d
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-06 08:26:27 +0000

    multiport

--- cloud/new_connect_from_cloud.sh
+++ cloud/new_connect_from_cloud.sh
@@ -3,6 +3,7 @@ echo "Usage: $0 [hostname]"
 exit 1
 fi
 
-PORT=$(cat /home/lpng/diagnostic_box_scripts/cloud/port)
+for PORT in $(cat /home/lpng/diagnostic_box_scripts/cloud/ports); do
 echo "Trying to connect to hostname $1 which we expect to be accessable on local port $PORT"
 ssh -p $PORT pi@localhost
+done
