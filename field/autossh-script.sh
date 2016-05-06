a68fa7fca7855bca2fce08c3371f75928c0dca5d
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-05 22:59:27 -0700

    log screen output gooder

--- field/autossh-script.sh
+++ field/autossh-script.sh
@@ -2,8 +2,6 @@
 
 export AUTOSSH_GATETIME=0
 
-PORT=$(/home/pi/diagnostic_box_scripts/field/machine_to_port.py $HOSTNAME)
-screen -dmSL autossh-gcloud autossh lpng@104.196.19.245 -R 0:localhost:22
-screen -dmSL autossh-aws autossh lpng@52.9.177.212 -R 0:localhost:22
-screen -dmSL old-autossh-gcloud autossh lpng@104.196.19.245 -R $PORT:localhost:22
-screen -dmSL old-autossh-aws autossh lpng@52.9.177.212 -R $PORT:localhost:22
+LOG_LOC_PREFIX="/home/pi/logfiles/screen/`date --iso-8601=minutes`"
+screen -dmSL autossh-gcloud "script '$LOG_LOC_PREFIX'_gcloud; autossh lpng@104.196.19.245 -R 0:localhost:22"
+screen -dmSL autossh-aws "'$LOG_LOC_PREFIX'_aws; autossh lpng@52.9.177.212 -R 0:localhost:22"
