5de5c6ab276e4b657418427b68faf66f8cd1dbda
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-11 23:45:27 -0700

    seperate autossh scripts for gcloud, aws. Both scp port info to cloud by moving port forwarding hack into those scripts

--- field/autossh-script.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-export AUTOSSH_GATETIME=0
-
-screen -dmSL autossh-gcloud autossh lpng@104.196.19.245 -R 0:localhost:22
-screen -dmSL autossh-aws autossh lpng@52.9.177.212 -R 0:localhost:22
--- field/make_tmp_folders.sh
+++
@@ -1,1 +0,0 @@
-mkdir /tmp/logs /tmp/logs/autoconnect /tmp/logs/autossh /tmp/logs/screen /tmp/logs/screen/gcloud /tmp/logs/screen/aws
