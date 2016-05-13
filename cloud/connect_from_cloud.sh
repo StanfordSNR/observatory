2655b61e9534f77d23d6f932febc6b26b9b4b643
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-13 08:24:05 +0000

    newer connect_from_cloud script

---
+++ cloud/connect_from_cloud.sh
@@ -0,0 +1,12 @@
+if [ "$#" -ne 1 ]; then
+echo "Usage: $0 [hostname]"
+exit 1
+fi
+
+PORT=$(cat /home/lpng/diagnostic_box_scripts/cloud/dynamic_ports/$1 2> /dev/null )
+if [ $PORT ]; then
+    echo "Trying to connect to hostname $1 which we expect to be accessable on local port $PORT"
+    ssh -p $PORT pi@localhost
+else
+    echo "Can't connect to hostname $1"
+fi
--- cloud/new_connect_from_cloud.sh
+++
@@ -1,9 +0,0 @@
-if [ "$#" -ne 1 ]; then
-echo "Usage: $0 [hostname]"
-exit 1
-fi
-
-for PORT in $(cat /home/lpng/diagnostic_box_scripts/cloud/ports); do
-echo "Trying to connect to hostname $1 which we expect to be accessable on local port $PORT"
-ssh -p $PORT pi@localhost
-done
