d9d6d69e122f3df206914c7a3efb7e764e143aad
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-13 01:08:43 -0700

    ff-only

--- home/git_pull_here_and_cloud.sh
+++ home/git_pull_here_and_cloud.sh
@@ -1,4 +1,4 @@
-git pull
-COMMAND="cd diagnostic_box_scripts && git pull"
+git pull --ff-only
+COMMAND="cd diagnostic_box_scripts && git pull --ff-only"
 ssh lpng@52.9.177.212 $COMMAND
 ssh lpng@104.196.19.245 $COMMAND
