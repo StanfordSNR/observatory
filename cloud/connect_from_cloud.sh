263a4ac7ad22a8543115da29c33afec6d441bcee
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-13 01:29:20 -0700

    typo

--- cloud/connect_from_cloud.sh
+++ cloud/connect_from_cloud.sh
@@ -5,7 +5,7 @@ fi
 
 PORT=$(cat /home/lpng/diagnostic_box_scripts/cloud/dynamic_ports/$1 2> /dev/null )
 if [ $PORT ]; then
-    echo "Trying to connect to hostname $1 which we expect to be accessable on local port $PORT"
+    echo "Trying to connect to hostname $1 which we expect to be accessible on local port $PORT"
     ssh -p $PORT pi@localhost
 else
     echo "Can't connect to hostname $1"
