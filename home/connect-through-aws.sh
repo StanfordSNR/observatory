e3695f95e87d8500afedc4c1020b1fa3e81d48e6
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-16 23:44:11 -0700

    connect-through-cloud scripts take hostname again

--- home/connect-through-aws.sh
+++ home/connect-through-aws.sh
@@ -1,1 +1,6 @@
-ssh -t lpng@52.192.246.2 ./diagnostic_box_scripts/cloud/connect_from_cloud.sh raspberrypi
+if [ "$#" -ne 1 ]; then
+echo "Usage: $0 [hostname]"
+exit 1
+fi
+
+ssh -t lpng@52.192.246.2 ./diagnostic_box_scripts/cloud/connect_from_cloud.sh $1
--- home/connect-through-gcloud.sh
+++ home/connect-through-gcloud.sh
@@ -1,1 +1,6 @@
-ssh -t lpng@23.251.141.221 ./diagnostic_box_scripts/cloud/connect_from_cloud.sh raspberrypi
+if [ "$#" -ne 1 ]; then
+echo "Usage: $0 [hostname]"
+exit 1
+fi
+
+ssh -t lpng@23.251.141.221 ./diagnostic_box_scripts/cloud/connect_from_cloud.sh $1
