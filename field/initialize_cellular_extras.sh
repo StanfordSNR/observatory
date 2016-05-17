10695df485b84707839230fb2c39b693ce15b259
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-16 20:44:12 -0700

    no more root for initialize scripts

--- field/initialize_cellular_extras.sh
+++ field/initialize_cellular_extras.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
-if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
-    exit
-fi
 
 cd /home/pi/diagnostic_box_scripts/field
-apt-get install -y usb-modeswitch wvdial
+sudo apt-get install -y usb-modeswitch wvdial
 # overwrite ethernet only cron jobs
 crontab cron_jobs_cellular
--- field/initialize_ethernet_only.sh
+++ field/initialize_ethernet_only.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
-if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
-    exit
-fi
 
 #raspi-config -- expand disk space
 
@@ -12,13 +8,13 @@ fi
 #ssh-known hosts and authorized_keys
 
 echo "installing git screen autossh"
-apt-get update
-apt-get install -y vim git screen autossh
+sudo apt-get update
+sudo apt-get install -y vim git screen autossh
 
 # Set up my dotfiles, also changes keyboard layout to US
 echo "setting up greg dotfiles"
 cd /home/pi
-git clone https://github.com/greghill/DotFiles.git && cd DotFiles && ./initialize.sh
+git clone https://github.com/greghill/DotFiles.git && cd DotFiles && sudo ./initialize.sh
 
 # Set up my diagnostic box scripts
 echo "getting diagnostic box scripts"
@@ -28,7 +24,7 @@ git clone https://github.com/StanfordLPNG/diagnostic_box_scripts
 cd diagnostic_box_scripts/field
 
 echo "changing timezone to Los Angeles"
-./change_timezone.sh
+sudo ./change_timezone.sh
 
 echo "Adding cron jobs"
 crontab cron_jobs_ethernet_only
@@ -36,9 +32,9 @@ crontab cron_jobs_ethernet_only
 # Change password from raspberry, requires user input
 passwd pi
 # Change hostname from raspberrypi, requires user input
-./change_hostname.sh
+sudo ./change_hostname.sh
 
-./make_readonly_filesystem.sh
+sudo ./make_readonly_filesystem.sh
 echo "will reboot on enter into readonly filesystem"
 read
-reboot
+sudo reboot
