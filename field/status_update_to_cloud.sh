5c4bb0e1160aa6c53af8153d096a0a5d1c246838
Greg D. Hill <greghill@cs.stanford.edu>
2016-05-17 14:40:30 -0700

    get rid of dumb extra quotation mark

--- field/status_update_to_cloud.sh
+++ field/status_update_to_cloud.sh
@@ -3,7 +3,7 @@ CONTENTS=$(
 date --iso-8601=minutes;
 echo -ne "\ndiagnostic_box_scripts HEAD="
 cd /home/pi/diagnostic_box_scripts && git rev-parse HEAD;
-/opt/vc/bin/vcgencmd measure_temp
+/opt/vc/bin/vcgencmd measure_temp | sed s/\'//g
 )
 echo $CONTENTS
 COMMAND="echo $CONTENTS >> diagnostic_box_scripts/cloud/status_logs/$(hostname)"
