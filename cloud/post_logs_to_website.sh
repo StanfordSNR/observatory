90873b93b87bbd49f009117a68b7be7252469046
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-09 22:05:35 +0000

    bug

--- cloud/post_logs_to_website.sh
+++ cloud/post_logs_to_website.sh
@@ -13,7 +13,7 @@ while : ; do
                 curl -i -X POST -d "$POST_REQ" https://network-observatory.herokuapp.com/post-measurement-box-checkin
             done < /tmp/toUpload
 
-            echo /tmp/toUpload >> ../uploaded_status_logs/$l
+            cat /tmp/toUpload >> ../uploaded_status_logs/$l
         done
         echo "starting over"
 done
