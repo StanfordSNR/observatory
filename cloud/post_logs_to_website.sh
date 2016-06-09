ce7ab77c0e12ef2a200d98d57949e6d114a8820c
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-09 14:06:00 -0700

    cron job to post logs from cloud box

--- cloud/post_logs_to_website.sh
+++ cloud/post_logs_to_website.sh
@@ -1,16 +1,18 @@
-#cd ~/diagnostic_box_scripts/cloud
-cd status_logs
-for l in *
-    do
-        echo $l
-        touch ../uploaded_status_logs/$l
-        comm -23 $l ../uploaded_status_logs/$l > /tmp/toUpload
+cd ~/diagnostic_box_scripts/cloud/status_logs
+while : ; do
+    sleep 5s
+    for l in *
+        do
+            echo $l
+            touch ../uploaded_status_logs/$l
+            comm -23 $l ../uploaded_status_logs/$l > /tmp/toUpload
 
-        while read line; do 
-            HOSTNAME=$l
-            POST_REQ='hostname='$l'&datetime='$(echo $line | sed 's/\ diagnostic_box_scripts\ HEAD=/\&head=/g; s/\ temp=/\&temp=/g; s/C$//g')
-            curl -i -X POST -d "$POST_REQ" https://network-observatory.herokuapp.com/post-measurement-box-checkin
-        done < /tmp/toUpload
+            while read line; do
+                HOSTNAME=$l
+                POST_REQ='hostname='$l'&datetime='$(echo $line | sed 's/\ diagnostic_box_scripts\ HEAD=/\&head=/g; s/\ temp=/\&temp=/g; s/C$//g')
+                curl -i -X POST -d "$POST_REQ" https://network-observatory.herokuapp.com/post-measurement-box-checkin
+            done < /tmp/toUpload
 
-        mv /tmp/toUpload ../uploaded_status_logs/$l
-    done
+            mv /tmp/toUpload ../uploaded_status_logs/$l
+        done
+done
