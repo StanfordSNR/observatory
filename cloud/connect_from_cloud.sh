08ce2c898354552c0c26b280b4afefb8922d469d
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-15 20:13:51 -0700

    connect from cloud use ~

--- cloud/connect_from_cloud.sh
+++ cloud/connect_from_cloud.sh
@@ -3,7 +3,7 @@ echo "Usage: $0 [hostname]"
 exit 1
 fi
 
-PORT=$(cat /home/lpng/diagnostic_box_scripts/cloud/dynamic_ports/$1 2> /dev/null )
+PORT=$(cat ~/diagnostic_box_scripts/cloud/dynamic_ports/$1 2> /dev/null )
 if [ $PORT ]; then
     echo "Trying to connect to hostname $1 which we expect to be accessible on local port $PORT"
     ssh -p $PORT pi@localhost
