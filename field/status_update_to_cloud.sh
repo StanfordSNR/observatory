e3e9ced9b58868ff4c8b3f652ca0f53d74da3e11
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-15 19:23:57 -0700

    status update to cloud using new style command restricted ssh

--- field/status_update_to_cloud.sh
+++ field/status_update_to_cloud.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
-CONTENTS=$(
-date --iso-8601=minutes;
-echo -ne "\ndiagnostic_box_scripts HEAD="
-cd /home/pi/diagnostic_box_scripts && git rev-parse HEAD;
-/opt/vc/bin/vcgencmd measure_temp | sed s/\'//g
-)
-echo $CONTENTS
-COMMAND="echo $CONTENTS >> diagnostic_box_scripts/cloud/status_logs/$(hostname)"
+COMMAND="web_checkin --git_head=$(git rev-parse HEAD) --temp=$(/opt/vc/bin/vcgencmd measure_temp | sed s/[^0-9.]*//g)"
+
 ssh lpng@52.192.246.2 $COMMAND &
 ssh lpng@23.251.141.221 $COMMAND
