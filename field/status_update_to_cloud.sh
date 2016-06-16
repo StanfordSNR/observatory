922c67fe29ba23090749e482a65b1bfa481e7db2
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-15 20:58:41 -0700

    fix small bug in cloud status update

--- field/status_update_to_cloud.sh
+++ field/status_update_to_cloud.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+cd ~/diagnostic_box_scripts #so we can get git head
 COMMAND="web_checkin --git_head=$(git rev-parse HEAD) --temp=$(/opt/vc/bin/vcgencmd measure_temp | sed s/[^0-9.]*//g)"
 
 ssh lpng@52.192.246.2 $COMMAND &
