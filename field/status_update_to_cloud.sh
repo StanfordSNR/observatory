e0acdc1036ba60103e45ccbb7810d16c4eb51a9c
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-18 16:41:30 -0700

    status update to cloud work without temperature script

--- field/status_update_to_cloud.sh
+++ field/status_update_to_cloud.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 cd ~/diagnostic_box_scripts #so we can get git head
-COMMAND="web_checkin --git_head=$(git rev-parse HEAD) --temp=$(/opt/vc/bin/vcgencmd measure_temp | sed s/[^0-9.]*//g)"
+
+# If temp script doesn't exist/work default to 0
+TEMP=$(/opt/vc/bin/vcgencmd measure_temp)
+if [ $? -eq 0 ]
+    then
+    TEMP=$(echo $TEMP | sed s/[^0-9.]*//g)
+else
+    TEMP=0
+fi
+
+COMMAND="web_checkin --git_head=$(git rev-parse HEAD) --temp=$TEMP"
 
 ssh lpng@52.192.246.2 $COMMAND &
 ssh lpng@23.251.141.221 $COMMAND
