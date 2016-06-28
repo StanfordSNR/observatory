e4501b09d402666442d176a22a9d1703c545c966
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-28 16:40:10 -0700

    update key for nepal4, fix bug in sync_authorized_keys script from folder re-org

--- cloud/sync_authorized_keys.sh
+++ cloud/sync_authorized_keys.sh
@@ -1,4 +1,4 @@
 cd diagnostic_box_scripts; git pull --ff-only;
-sort -u ~/.ssh/authorized_keys ~/diagnostic_box_scripts/field/authorized_keys > /tmp/authorized_keys;
+sort -u ~/.ssh/authorized_keys ~/diagnostic_box_scripts/field/initialization/authorized_keys > /tmp/authorized_keys;
 mv /tmp/authorized_keys ~/.ssh/authorized_keys;
 echo keys synced
