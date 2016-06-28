c6452e46bab491fdb2e486a39c9ccd2c14da95d6
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-27 19:27:57 -0700

    Script and cron jobs to roll-back a git pull after a reboot if not cancelled by user

---
+++ field/git_automatic_rollback.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+fi
+
+sleep 4m
+cd ~/diagnostic_box_scripts/field
+./mount_readwrite.sh
+git reset --hard master@{"30 minutes ago"}
+crontab root_cron_jobs
