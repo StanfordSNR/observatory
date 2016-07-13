8cd71632401ed40ce5ee5bbd8519ebc81668c24e
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-13 16:47:49 -0700

    rename autoconnect to cellular_autoconnect, move cron job to root cron jobs from user

--- field/make_tmp_dirs.sh
+++ field/make_tmp_dirs.sh
@@ -1,1 +1,1 @@
-mkdir /tmp/aws_logs /tmp/gcloud_logs /tmp/gcloud_logs/autossh /tmp/aws_logs/autossh /tmp/gcloud_logs/screen /tmp/aws_logs/screen /tmp/autoconnect_logs
+mkdir /tmp/aws_logs /tmp/gcloud_logs /tmp/gcloud_logs/autossh /tmp/aws_logs/autossh /tmp/gcloud_logs/screen /tmp/aws_logs/screen /tmp/cellular_autoconnect_logs
