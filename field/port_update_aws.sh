25c9b4063cc13827e09d37b5bfcae272e4430597
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-17 19:26:11 -0700

    add some more comments for port_update scripts and sleep for longer in the loop

--- field/port_update_aws.sh
+++ field/port_update_aws.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
+# ssh -R prints locally which port it is bound to on the remote machine. This script gets this port number from the log for the autossh script and gives it to the cloud server.
+
 cd /tmp/aws_logs
 AWS_ADDRESS=lpng@52.192.246.2
 
 OLD_PORT=0
 while : ; do
     PORT=$(tac autossh_aws.log | grep -o "Allocated port [0-9]*" -m 1 | cut -d ' ' -f 3)
-    if [ "$OLD_PORT" != "$PORT" ]
+    if [ "$OLD_PORT" != "$PORT" ] # if port has changed from the last one we updated
     then
         ssh $AWS_ADDRESS "set_port --port $PORT"
     fi
     OLD_PORT=$PORT
-    sleep 1
+    sleep 5
 done
