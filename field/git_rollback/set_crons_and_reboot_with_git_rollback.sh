7309039d27442e5d130c2ab414449493d0f0d04a
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-21 16:11:18 -0700

    set git rollback script require root

--- field/git_rollback/set_crons_and_reboot_with_git_rollback.sh
+++ field/git_rollback/set_crons_and_reboot_with_git_rollback.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -xe
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+fi
+
 # run after a git pull to setup cron jobs and rollback if not cancelled on reboot
-crontab /home/pi/diagnostic_box_scripts/field/initialization/user_cron_jobs
+crontab -u pi /home/pi/diagnostic_box_scripts/field/initialization/user_cron_jobs
 grep -v '/sbin/shutdown' /home/pi/diagnostic_box_scripts/field/initialization/root_cron_jobs > /tmp/newcrontab # put crontab lines that aren't shutdown into temp file
 cat /home/pi/diagnostic_box_scripts/field/git_rollback/root_git_rollback_cron_job >> /tmp/newcrontab # add git rollback lines
-sudo crontab /tmp/newcrontab
-sudo reboot
+crontab /tmp/newcrontab
+/sbin/shutdown -r
