a157e6eaa6a0b07ef06aa2acb8003f4afd3d6be6
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-21 14:31:02 -0700

    bugfixes for rollback setup script

--- field/git_rollback/git_pull_reboot_and_rollback.sh
+++ field/git_rollback/git_pull_reboot_and_rollback.sh
@@ -1,8 +1,9 @@
-#! /bin/bash -e #abort if any command turns nonzero
-sudo ./mount_readwrite.sh
+#!/bin/bash -xe
+exit 1
+sudo /home/pi/diagnostic_box_scripts/field/mount_readwrite.sh
 git pull --ff-only
 crontab /home/pi/diagnostic_box_scripts/field/initialization/user_cron_jobs
 sudo crontab -l > /tmp/newcrontab
-echo root_git_rollback_cron_job >> /tmp/newcrontab
+cat /home/pi/diagnostic_box_scripts/field/git_rollback/root_git_rollback_cron_job >> /tmp/newcrontab
 sudo crontab /tmp/newcrontab
 sudo reboot
