2f48427ea19b986e8f603ca1ed9168213a09d3be
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-21 16:02:15 -0700

    take hourly reboot out of git rollback cron job

--- field/git_rollback/git_pull_reboot_and_rollback.sh
+++ field/git_rollback/git_pull_reboot_and_rollback.sh
@@ -2,7 +2,7 @@
 sudo /home/pi/diagnostic_box_scripts/field/mount_readwrite.sh
 git pull --ff-only
 crontab /home/pi/diagnostic_box_scripts/field/initialization/user_cron_jobs
-sudo crontab -l > /tmp/newcrontab
-cat /home/pi/diagnostic_box_scripts/field/git_rollback/root_git_rollback_cron_job >> /tmp/newcrontab
+grep -v '/sbin/shutdown' /home/pi/diagnostic_box_scripts/field/initialization/root_cron_jobs > /tmp/newcrontab # put crontab lines that aren't shutdown into temp file
+cat /home/pi/diagnostic_box_scripts/field/git_rollback/root_git_rollback_cron_job >> /tmp/newcrontab # add git rollback lines
 sudo crontab /tmp/newcrontab
 sudo reboot
