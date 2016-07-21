069e93746c5905314f26bd43c23f24c79db720da
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-21 16:09:33 -0700

    rename rollback script and assume filesystem is mounted readwrite and git pull already completed

--- field/git_rollback/set_crons_and_reboot_with_git_rollback.sh
+++ field/git_rollback/set_crons_and_reboot_with_git_rollback.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -xe
-sudo /home/pi/diagnostic_box_scripts/field/mount_readwrite.sh
-git pull --ff-only
+# run after a git pull to setup cron jobs and rollback if not cancelled on reboot
 crontab /home/pi/diagnostic_box_scripts/field/initialization/user_cron_jobs
 grep -v '/sbin/shutdown' /home/pi/diagnostic_box_scripts/field/initialization/root_cron_jobs > /tmp/newcrontab # put crontab lines that aren't shutdown into temp file
 cat /home/pi/diagnostic_box_scripts/field/git_rollback/root_git_rollback_cron_job >> /tmp/newcrontab # add git rollback lines
