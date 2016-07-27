982310f43e87d1c80c5109452dc11f0704a4a5e8
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-27 11:16:49 -0700

    add symbolic link to tempfs for ppp pap-secrets and chap-secrets

--- field/initialization/initialize_cellular_extras.sh
+++ field/initialization/initialize_cellular_extras.sh
@@ -2,3 +2,5 @@
 
 sudo apt-get install -y vim usb-modeswitch wvdial
 sudo cp ~/diagnostic_box_scripts/field/cellular/wvdial.conf /etc/wvdial.conf
+sudo ln -s /etc/ppp/pap-secrets /tmp/.ppp.pap-secrets
+sudo ln -s /etc/ppp/chap-secrets /tmp/.ppp.chap-secrets
