9bd14fd9736f3fabe7f3a043a78f8679ab368862
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-26 19:42:55 -0700

    separate carriers into own wvdial script

--- field/cellular/automated_cellular_connection.sh
+++ field/cellular/automated_cellular_connection.sh
@@ -13,8 +13,10 @@ while : ; do
     sleep 2s
     /home/pi/diagnostic_box_scripts/field/cellular/generic_huawei_modeswitch.sh
     sleep 5s
-    wvdial E397
-    wvdial E3531
+    wvdial att E397
+    wvdial tmobile E397
+    wvdial att E3531
+    wvdial tmobile E3531
 
     # if we fall to here make sure to restart autossh connections
     pkill autossh --signal USR1
