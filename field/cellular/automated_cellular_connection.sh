c7272c16e6961ef36cb97f3c2357b2c07e2bff33
Greg D. Hill <greghill@cs.stanford.edu>
2016-07-26 19:37:53 -0700

    make automated cellular script -x and get rid of dhclient hack

--- field/cellular/automated_cellular_connection.sh
+++ field/cellular/automated_cellular_connection.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -x
 if [ "$EUID" -ne 0 ]
     then echo "Please run as root"
     exit
@@ -10,21 +10,12 @@ while : ; do
         echo "no huawei devices found"
         sleep 5s
     done
-    echo sleep 2s
     sleep 2s
-    echo generic_huawei_modeswitch.sh
     /home/pi/diagnostic_box_scripts/field/cellular/generic_huawei_modeswitch.sh
-    echo sleep 5s
     sleep 5s
-    echo  wvdial E397
-     wvdial E397
-    echo  wvdial E531
-     wvdial E3531
-    #temp hack for dhcp
-    echo  dhclient eth1
-    dhclient eth1
+    wvdial E397
+    wvdial E3531
 
     # if we fall to here make sure to restart autossh connections
-    echo pkill autossh --signal USR1
     pkill autossh --signal USR1
 done
