83ea441fedfc04c1cd9e0623989585e27eabff9e
Greg D. Hill <greghill@cs.stanford.edu>
2016-08-04 01:00:26 -0700

    safaricom pin script separate

--- field/cellular/automated_cellular_connection.sh
+++ field/cellular/automated_cellular_connection.sh
@@ -13,7 +13,8 @@ while : ; do
     sleep 2s
     /home/pi/diagnostic_box_scripts/field/cellular/generic_huawei_modeswitch.sh
     sleep 5s
-    wvdial safaricom E3531
+    wvdial E3531 safaricom pin
+    wvdial E3531 safaricom
 
     # if we fall to here make sure to restart autossh connections
     pkill autossh --signal USR1
