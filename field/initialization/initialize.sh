6fe275e1c1eef5f0c8fb6f5d0c0927822b0e6867
Greg D. Hill <greghill@cs.stanford.edu>
2016-08-03 23:33:35 -0700

    initialize.sh pull before committing

--- field/initialization/initialize.sh
+++ field/initialization/initialize.sh
@@ -51,6 +51,7 @@ sudo ./change_hostname.sh
 # make default ssh key that is command restricted and add to repo
 cat /dev/zero | ssh-keygen -q -N ""
 RESTRICTED_KEY="command=\"~/diagnostic_box_scripts/cloud/cloud_util.py $(cat /etc/hostname) \$SSH_ORIGINAL_COMMAND\", $(cat ~/.ssh/id_rsa.pub)"
+git pull --ff-only # make sure we aren't behind already
 echo $RESTRICTED_KEY >> authorized_keys
 git add authorized_keys
 git commit -m "adding command restricted key for $(cat /etc/hostname) for cloud servers to add to authorized_keys"
