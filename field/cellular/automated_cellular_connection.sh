9a018fbce3409ce9d224b2e74f1668e2e240b100
Greg D. Hill <greghill@cs.stanford.edu>
2016-08-04 00:52:08 -0700

    only connect to safaricom in automated cellular connection

--- field/cellular/automated_cellular_connection.sh
+++ field/cellular/automated_cellular_connection.sh
@@ -13,10 +13,7 @@ while : ; do
     sleep 2s
     /home/pi/diagnostic_box_scripts/field/cellular/generic_huawei_modeswitch.sh
     sleep 5s
-    wvdial att E397
-    wvdial tmobile E397
-    wvdial att E3531
-    wvdial tmobile E3531
+    wvdial safaricom E3531
 
     # if we fall to here make sure to restart autossh connections
     pkill autossh --signal USR1
