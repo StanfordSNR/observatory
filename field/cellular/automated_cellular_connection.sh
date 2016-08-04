61ce7b48154bb643e23205c1d4d6166858a63e0c
Greg D. Hill <greghill@cs.stanford.edu>
2016-08-04 02:11:02 -0700

    remove pin use

--- field/cellular/automated_cellular_connection.sh
+++ field/cellular/automated_cellular_connection.sh
@@ -13,8 +13,6 @@ while : ; do
     sleep 5s
     /home/pi/diagnostic_box_scripts/field/cellular/generic_huawei_modeswitch.sh
     sleep 10s
-    wvdial E3531 safaricom pin
-    sleep 5s
     wvdial E3531 safaricom
 
     # if we fall to here make sure to restart autossh connections
