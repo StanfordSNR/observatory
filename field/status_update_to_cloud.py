24a681d146ac7579d8cc8360e1fc883c2cb0d085
Greg D. Hill <greghill@cs.stanford.edu>
2016-08-25 15:55:41 -0700

    status_update_to_cloud.py works now

--- field/status_update_to_cloud.py
+++ field/status_update_to_cloud.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 import argparse
-from subprocess import call
+from subprocess import call, check_output
 import ipaddress
 
 parser = argparse.ArgumentParser(description='Get various statistics to send to cloud box')
@@ -11,19 +11,22 @@ parser.add_argument('cloud_ip', type=ipaddress.ip_address, help='IP address of c
 
 args = parser.parse_args()
 
-status_update_cmd = ["ssh", args.cloud_username + "@" + str(args.cloud_ip), "--allowed-command", "/home/lpng/cloud/status_update_to_webserver.py"]
+status_update_command = ['ssh', args.cloud_username + '@' + str(args.cloud_ip), 'allowed-command --command /home/lpng/diagnostic_box_scripts/cloud/status_update_to_webserver.py']
+
+hostname = check_output("hostname")
+status_update_command += [hostname]
 
 # Only run if temp command exists
-temp_results = call("/opt/vc/bin/vcgencmd measure_temp")
+temp_results = check_output("/opt/vc/bin/vcgencmd measure_temp | sed s/[^0-9.]*//g", shell=True)
 status_update_command += ["--temp", temp_results]
 
-git_head_results = call("cd ~/diagnostic_box_scripts && git rev-parse HEAD", shell=True)
+git_head_results = check_output("cd ~/diagnostic_box_scripts && git rev-parse HEAD", shell=True)
 status_update_command += ["--git-head", git_head_results]
 
-uptime_results = call("uptime")
+uptime_results = check_output("uptime")
 status_update_command += ["--uptime", uptime_results]
 
-public_ip = call("dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | sed 's/\"//g'")
+public_ip = check_output("dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | sed 's/\"//g'", shell=True)
 status_update_command += ["--public-ip", public_ip]
 
 call(status_update_command)
