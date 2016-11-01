5eefdc5042038278aafde11aa2be43ea3b3a80ac
Greg D. Hill <greghill@cs.stanford.edu>
2016-11-01 13:14:01 -0700

    add dialing scripts for columbia

---
+++ cellular/wvdial_columbia.sh
@@ -0,0 +1,2 @@
+wvdial --config=/home/pi/diagnostic_box_scripts/field/cellular/wvdial.conf E3531 claro_columbia
+wvdial --config=/home/pi/diagnostic_box_scripts/field/cellular/wvdial.conf E397  claro_columbia
