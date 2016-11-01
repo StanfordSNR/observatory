f66b89d1b04985ddeecbc45b6e01de0d9d2c0996
Greg D. Hill <greghill@cs.stanford.edu>
2016-11-01 12:12:17 -0700

    add dialing scripts for mexico

---
+++ cellular/wvdial_mexico.sh
@@ -0,0 +1,2 @@
+wvdial --config=/home/pi/diagnostic_box_scripts/field/cellular/wvdial.conf E3531 telcel_mexico
+wvdial --config=/home/pi/diagnostic_box_scripts/field/cellular/wvdial.conf E397  telcel_mexico
