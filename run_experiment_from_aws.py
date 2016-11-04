235050498e732fb8ba04f83dfc19c8ae271111d4
Greg D. Hill <greghill@cs.stanford.edu>
2016-11-04 01:39:34 +0000

    run_experiment_from_aws.py: new argument to define where the experiment is being initiated from

--- run_experiment_from_aws.py
+++ run_experiment_from_aws.py
@@ -7,7 +7,12 @@ from datetime import datetime
 from subprocess import Popen, PIPE, check_call, check_output
 import argparse
 
-sites = {
+sources = {
+    'aws brazil': '52.67.123.5',
+    'aws california': '52.9.40.135',
+}
+
+destinations = {
     'stanford': 'greg@171.66.3.239',
     'brazil': 'pi@177.234.144.122',
     'columbia': 'pi@138.121.201.58',
@@ -15,29 +20,27 @@ sites = {
     'mexico': 'pi@143.255.56.146'
 }
 
-# location_and_external_ip = ('aws brazil', '52.67.123.5')
-location_and_external_ip = ('aws california', '52.9.40.135')
-
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('site_name', type=str, choices=sites.keys())
+    parser.add_argument('source', type=str, choices=sources.keys())
+    parser.add_argument('destination', type=str, choices=destinations.keys())
     args = parser.parse_args()
 
     test_dir = os.path.expanduser('~/pantheon/test/')
     os.chdir(test_dir)
 
     local_head = check_output(['git', 'rev-parse', 'HEAD'])
-    remote_head = check_output(['ssh', sites[args.site_name], 'git', '-C',
-                                'pantheon', 'rev-parse', 'HEAD'])
+    remote_head = check_output(['ssh', destinations[args.destination], 'git',
+                                '-C', 'pantheon', 'rev-parse', 'HEAD'])
     assert (local_head == remote_head
             ), "pantheon versions differ between local (%s) and remote (%s)" \
         % (local_head[:-1], remote_head[:-1])
 
     local_submodules = check_output(['git', 'submodule', 'summary'])
-    remote_submodules = check_output(['ssh', sites[args.site_name], 'git',
-                                      '-C', 'pantheon', 'submodule', 'summary'
-                                      ])
+    remote_submodules = check_output(['ssh', destinations[args.destination],
+                                      'git', '-C', 'pantheon', 'submodule',
+                                      'summary'])
     assert (local_submodules == remote_submodules
             ), "pantheon submodule versions differ between local and remote " \
                "repositories"
@@ -47,18 +50,18 @@ def main():
     check_call('rm -f *.png', shell=True)
     check_call('rm -f *.xz ', shell=True)
 
-    cmd = './run.py -t 30 -r ' + sites[args.site_name] + ':pantheon ' + \
+    cmd = './run.py -t 30 -r ' + destinations[args.destination] + ':pantheon '\
           '--tunnel-server local  --sender-side remote --random-order ' \
-          '--local-addr ' + location_and_external_ip[1] + ' ' + \
-          '--local-info "%s" ' % location_and_external_ip[0] + \
-          '--remote-interface ppp0 --remote-info ' + args.site_name
+          '--local-addr ' + sources[args.source] + ' ' + \
+          '--local-info "%s" ' % args.source + \
+          '--remote-interface ppp0 --remote-info "%s"' % args.destination
 
     print(cmd)
     check_call(cmd, shell=True)
 
     date = datetime.utcnow()
     date = date.replace(microsecond=0).isoformat().replace(':', '-')
-    s3_url = 's3://stanford-pantheon/real-world-results/' + args.site_name \
+    s3_url = 's3://stanford-pantheon/real-world-results/' + args.destination \
              + '/'
     src_file = date + '_logs.xz'
     dst_file = s3_url + src_file
