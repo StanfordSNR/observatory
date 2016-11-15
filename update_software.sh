55af7bffec42fe7b87bc8a49226d39a2942a6a2c
Greg D. Hill <greghill@cs.stanford.edu>
2016-11-15 00:17:48 +0000

    update_software.sh: add awscli, ntpdate and remove cellular stuff

--- update_software.sh
+++ update_software.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get -y dist-upgrade
-sudo apt-get install -y usb-modeswitch wvdial traceroute mosh dnsutils python-dev python-pip ntp
+sudo apt-get install -y traceroute mosh dnsutils python-dev python-pip ntp ntpdate awscli
 sudo pip install requests
 sudo pip install matplotlib
