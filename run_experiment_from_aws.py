3513cceae5d27a0b80e1c73e3892845806fc0c6f
Greg D. Hill <greghill@cs.stanford.edu>
2016-11-29 00:59:42 +0000

    run_experiment_from_aws.py: fix comma bug

--- run_experiment_from_aws.py
+++ run_experiment_from_aws.py
@@ -17,7 +17,7 @@ destinations = {
     'Brazil': 'pi@177.234.144.122',
     'Colombia': 'pi@138.121.201.58',
     'India': 'pi@109.73.164.122',
-    'Mexico': 'pi@143.255.56.146'
+    'Mexico': 'pi@143.255.56.146',
     'China': 'yanyu@101.6.97.145'
 }
 
