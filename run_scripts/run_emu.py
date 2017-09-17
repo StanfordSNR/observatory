923782c9f7d20267d8d5fd3db465a781a68fd722
Francis Y. Yan <fyy@cs.stanford.edu>
2017-09-17 07:31:41 +0000

    Update calibrated emulators

--- run_scripts/run_emu.py
+++ run_scripts/run_emu.py
@@ -10,8 +10,8 @@ def run(args):
     assistant = path.join(project_root.DIR, 'assistant.py')
     console = path.join(project_root.DIR, 'console.py')
 
-    check_call([assistant, '--aws-servers', '-c', 'cleanup'])
-    check_call([assistant, '--aws-servers', '-c', 'setup'])
+    check_call([assistant, '--emu-servers', '-c', 'cleanup'])
+    check_call([assistant, '--emu-servers', '-c', 'setup'])
 
     base_cmd = [console, 'emu', '--all', '--run-times', '10']
     if args.flows > 1:
@@ -19,24 +19,28 @@ def run(args):
 
     procs = []
 
-    # China cellular to AWS Korea
-    cmd =  base_cmd + ['--id', 'China ppp0 to AWS Korea', '--desc', 'Calibrated to the real path from China cellular to AWS Korea (2016-12-30T21-38-China-ppp0-to-AWS-Korea-10-runs)', '--uplink-trace', '~/pantheon/test/6.25mbps.trace', '--downlink-trace', '~/pantheon/test/6.25mbps.trace', '--prepend-mm-cmds', 'mm-delay 152 mm-loss uplink 0.0025', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=362', 'aws_korea']
+    # Nepal to AWS India
+    cmd =  base_cmd + ['--id', 'Nepal to AWS India', '--desc', 'Calibrated to the real path from Nepal to AWS India (http://pantheon.stanford.edu/result/188/)', '--uplink-trace', '~/pantheon/test/0.57mbps-poisson.trace', '--downlink-trace', '~/pantheon/test/0.57mbps-poisson.trace', '--prepend-mm-cmds', 'mm-delay 28 mm-loss uplink 0.0477', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=14', 'emu_1']
     procs.append(Popen(cmd))
 
-    # AWS Brazil to Brazil
-    cmd =  base_cmd + ['--id', 'AWS Brazil to Brazil', '--desc', 'Calibrated to the real path from AWS Brazil to Brazil (2017-01-04T07-24-AWS-Brazil-1-to-Brazil-10-runs)', '--uplink-trace', '~/pantheon/test/97.11mbps.trace', '--downlink-trace', '~/pantheon/test/97.11mbps.trace', '--prepend-mm-cmds', 'mm-delay 1', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=366', 'aws_brazil_1']
+    # Mexico ppp0 to AWS California
+    cmd =  base_cmd + ['--id', 'Mexico ppp0 to AWS California', '--desc', 'Calibrated to the real path from Mexico cellular to AWS California (http://pantheon.stanford.edu/result/196/)', '--uplink-trace', '~/pantheon/test/2.64mbps-poisson.trace', '--downlink-trace', '~/pantheon/test/2.64mbps-poisson.trace', '--prepend-mm-cmds', 'mm-delay 88', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=130', 'emu_2']
     procs.append(Popen(cmd))
 
-    # AWS Brazil to Colombia cellular
-    cmd =  base_cmd + ['--id', 'AWS Brazil to Colombia ppp0', '--desc', 'Calibrated to the real path from AWS Brazil to Colombia cellular (2016-12-30T22-50-AWS-Brazil-2-to-Colombia-ppp0-10-runs)', '--uplink-trace', '~/pantheon/test/5.65mbps.trace', '--downlink-trace', '~/pantheon/test/5.65mbps.trace', '--prepend-mm-cmds', 'mm-delay 88 mm-loss uplink 0.0026 mm-loss downlink 0.0001', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=3665', 'aws_brazil_2']
+    # AWS Brazil to Colombia ppp0
+    cmd =  base_cmd + ['--id', 'AWS Brazil to Colombia ppp0', '--desc', 'Calibrated to the real path from AWS Brazil to Colombia cellular (http://pantheon.stanford.edu/result/339/)', '--uplink-trace', '~/pantheon/test/3.04mbps-poisson.trace', '--downlink-trace', '~/pantheon/test/3.04mbps-poisson.trace', '--prepend-mm-cmds', 'mm-delay 130', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=426', 'emu_3']
     procs.append(Popen(cmd))
 
     # India to AWS India
-    cmd =  base_cmd + ['--id', 'India to AWS India', '--desc', 'Calibrated to the real path from India to AWS India (2017-01-02T03-54-India-to-AWS-India-10-runs)', '--uplink-trace', '~/pantheon/test/117.65mbps.trace', '--downlink-trace', '~/pantheon/test/117.65mbps.trace', '--prepend-mm-cmds', 'mm-delay 13', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=144', 'aws_india_1']
+    cmd =  base_cmd + ['--id', 'India to AWS India', '--desc', 'Calibrated to the real path from India to AWS India (http://pantheon.stanford.edu/result/251/)', '--uplink-trace', '~/pantheon/test/100.42mbps.trace', '--downlink-trace', '~/pantheon/test/100.42mbps.trace', '--prepend-mm-cmds', 'mm-delay 27', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=173', 'emu_4']
     procs.append(Popen(cmd))
 
-    # Nepal to AWS India
-    cmd =  base_cmd + ['--id', 'Nepal to AWS India', '--desc', 'Calibrated to the real path from Nepal to AWS India (2017-01-03T21-30-Nepal-to-AWS-India-10-runs)', '--uplink-trace', '~/pantheon/test/13.38mbps.trace', '--downlink-trace', '~/pantheon/test/13.38mbps.trace', '--prepend-mm-cmds', 'mm-delay 32 mm-loss uplink 0.003', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=37', 'aws_india_2']
+    # AWS Korea to China
+    cmd =  base_cmd + ['--id', 'AWS Korea to China', '--desc', 'Calibrated to the real path from AWS Korea to China (http://pantheon.stanford.edu/result/361/)', '--uplink-trace', '~/pantheon/test/77.72mbps.trace', '--downlink-trace', '~/pantheon/test/77.72mbps.trace', '--prepend-mm-cmds', 'mm-delay 51 mm-loss uplink 0.0006', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=94', 'emu_5']
+    procs.append(Popen(cmd))
+
+    # AWS California to Mexico
+    cmd =  base_cmd + ['--id', 'AWS California to Mexico', '--desc', 'Calibrated to the real path from AWS California to Mexico (http://pantheon.stanford.edu/result/353/)', '--uplink-trace', '~/pantheon/test/114.68mbps.trace', '--downlink-trace', '~/pantheon/test/114.68mbps.trace', '--prepend-mm-cmds', 'mm-delay 45', '--extra-mm-link-args', '--uplink-queue=droptail --uplink-queue-args=packets=450', 'emu_6']
     procs.append(Popen(cmd))
 
     wait_procs(procs)
