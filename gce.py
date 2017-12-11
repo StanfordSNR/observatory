0a8cb1baa66b69b8526c801511ba0f261d580e6a
Francis Y. Yan <fyy@cs.stanford.edu>
2017-12-11 07:21:07 +0000

    Add gce to automatically start and stop GCE instances

---
+++ gce.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import yaml
+import argparse
+from helpers.helpers import call, check_output, parse_config
+
+
+def main():
+    config = parse_config()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', choices=['start', 'stop', 'status'])
+    args = parser.parse_args()
+
+    procs = []
+    for server, server_cfg in config['gce_servers'].iteritems():
+        instance_name = server_cfg['name']
+        zone = server_cfg['zone']
+
+        if args.action == 'start':
+            procs.append(Popen(['gcloud', 'compute', 'instances', 'start',
+                               instance_name, '--zone', zone]))
+        elif args.action == 'stop':
+            procs.append(Popen(['gcloud', 'compute', 'instances', 'stop',
+                               instance_name, '--zone', zone]))
+        elif args.action == 'status':
+            d = yaml.load(check_output(
+                    ['gcloud', 'compute', 'instances', 'describe',
+                     instance_name, '--zone', zone]))
+            sys.stdout.write(d['status'] + '\n')
+
+    for proc in procs:
+        proc.wait()
+
+
+if __name__ == '__main__':
+    main()
